efa1d289aade8f18c8c79eb43389d064fd7ff174
Sebastian Orlowski <sebastian.orlowski420@gmail.com>
2020-09-21 10:08:27 +0200

    Start app

---
+++ src/main/java/pl/orlowski/sebastian/forumspring/ForumspringApplication.java
@@ -0,0 +1,13 @@
+package pl.orlowski.sebastian.forumspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/orlowski/sebastian/forumspring/ForumspringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.orlowski.sebastian.forumspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
