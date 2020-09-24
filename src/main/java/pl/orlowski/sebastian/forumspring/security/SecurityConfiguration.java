9c74ffcb3cdbd17b9afe09484babd52ce9c7665e
Sebastian Orlowski <sebastian.orlowski420@gmail.com>
2020-09-24 18:30:56 +0200

    Add security options

--- src/main/java/pl/orlowski/sebastian/forumspring/security/SecurityConfiguration.java
+++ src/main/java/pl/orlowski/sebastian/forumspring/security/SecurityConfiguration.java
@@ -1,14 +1,36 @@
 package pl.orlowski.sebastian.forumspring.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("123456")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -26,8 +48,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/login", "/register", "/forgot").permitAll()
-                .antMatchers("/login","/topics","/inscription","/user").hasAnyRole()
-                .antMatchers("/admin").access("admin");
+                .antMatchers("/register", "/forgot").permitAll()
+                .antMatchers("/topics","/inscription","/user").hasRole("user")
+                .anyRequest().hasRole("admin")
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll()
+                .logoutSuccessUrl("/login");
     }
 }
