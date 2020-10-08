593b480dc06919ebd17ca0cd03075300983bf1aa
Sebastian Orlowski <sebastian.orlowski420@gmail.com>
2020-10-08 10:22:45 +0200

    Add impl to userservice

--- src/main/java/pl/orlowski/sebastian/forumspring/user/UserServiceImpl.java
+++ src/main/java/pl/orlowski/sebastian/forumspring/user/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package pl.orlowski.sebastian.forumspring.user;
 
+import com.fasterxml.jackson.databind.module.SimpleAbstractTypeResolver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -9,6 +12,8 @@ import pl.orlowski.sebastian.forumspring.repository.UserRepository;
 import pl.orlowski.sebastian.forumspring.service.UserService;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -30,14 +35,19 @@ public class UserServiceImpl implements UserService {
         return userRepository.save(user);
     }
 
-    /* Load user by login */
+    /* Login user */
     @Override
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
         User user = userRepository.findByLogin(login);
         if (user != null) {
-            return (UserDetails) user;
+            return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
         } throw new UsernameNotFoundException("Unknown user");
     }
 
     /* Transfer roles to authorities */
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+//        Put roles into stream then map the role, we put role to the security provide class
+//        simplegrantedauthority and we pass roles name to this object and finally we collected stream to the list
+       return roles.stream().map(role -> new SimpleGrantedAuthority(role.getRole())).collect(Collectors.toList());
+    }
 }
