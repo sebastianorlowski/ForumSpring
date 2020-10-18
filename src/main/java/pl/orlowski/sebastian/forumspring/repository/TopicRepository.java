74ed4e520d51d5a30b283551e82dadce54c49b0f
Sebastian Orlowski <sebastian.orlowski420@gmail.com>
2020-10-18 12:56:53 +0200

    Add data connection to entity

--- src/main/java/pl/orlowski/sebastian/forumspring/repository/TopicRepository.java
+++ src/main/java/pl/orlowski/sebastian/forumspring/repository/TopicRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.orlowski.sebastian.forumspring.topic.Topic;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
-    Topic findByName(String name);
 
 }
