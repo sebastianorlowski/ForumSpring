c848b4e2f20b33063a82e80e06454136a15461c1
Sebastian Orlowski <sebastian.orlowski420@gmail.com>
2020-10-18 11:48:16 +0200

    Add repositories

---
+++ src/main/java/pl/orlowski/sebastian/forumspring/repository/InscriptionRepository.java
@@ -0,0 +1,8 @@
+package pl.orlowski.sebastian.forumspring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.orlowski.sebastian.forumspring.topic.Inscription;
+
+public interface InscriptionRepository extends JpaRepository<Inscription, Long> {
+    Inscription findByTopicAndId(String name, Long id);
+}
--- src/main/java/pl/orlowski/sebastian/forumspring/repository/TopicRepository.java
+++ src/main/java/pl/orlowski/sebastian/forumspring/repository/TopicRepository.java
@@ -1,4 +1,9 @@
 package pl.orlowski.sebastian.forumspring.repository;
 
-public interface TopicRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.orlowski.sebastian.forumspring.topic.Topic;
+
+public interface TopicRepository extends JpaRepository<Topic, Long> {
+    Topic findByName(String name);
+
 }
